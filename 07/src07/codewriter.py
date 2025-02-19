45b55a2dc5e27d762dcebcc28a49c774cd4a244e
Occccn <big.island.yz@gmail.com>
2025-02-19 11:10:52 +0900

    test

--- 07/src07/codewriter.py
+++ 07/src07/codewriter.py
@@ -9,10 +9,16 @@ class CodeWriter:
         pass
 
     def writePushPop(self, command: Literal["C_PUSH", "C_POP"], segment: str, index: int) -> None:
+        # RAM[0]の位置にstaclの最上位のアドレスがある
+        # local/argument/this/thatの最上位アドレスはRAM[1]~RAM[4]に格納されている
+        # constantはその値をスタックに格納する
+        # temp iはRAM[5+i]に格納されている
+        # pointer 0はthis, 1はthatにアクセス
+
         # if command == "C_PUSH":
         #     if segment == "constant":
-                
-        # pass
+
+        pass
 
     def close(self) -> None:
         self.f_stream.close()
