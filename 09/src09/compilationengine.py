43dece1160a5fa627e9500adfc3fd5193fae2c1b
Occccn <big.island.yz@gmail.com>
2025-03-16 16:38:40 +0900

    9章init

---
+++ 09/src09/compilationengine.py
@@ -0,0 +1,45 @@
+class CompilationEngine:
+    def __init__(self, input_file, output_file):
+        pass
+
+    def compileClass(self):
+        pass
+
+    def compileClassVarDec(self):
+        pass
+
+    def compileSubroutine(self):
+        pass
+
+    def compileParameterList(self):
+        pass
+
+    def compileVarDec(self):
+        pass
+
+    def compileStatements(self):
+        pass
+
+    def compileDo(self):
+        pass
+
+    def compileLet(self):
+        pass
+
+    def compileWhile(self):
+        pass
+
+    def compileReturn(self):
+        pass
+
+    def compileIf(self):
+        pass
+
+    def compileExpression(self):
+        pass
+
+    def compileTerm(self):
+        pass
+
+    def compileExpressionList(self):
+        pass
